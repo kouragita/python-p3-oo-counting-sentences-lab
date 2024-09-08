41ca9bd589f1fd6e25e7ab084cac20640b68a8a5
wilson mwangi waweru <kouragitam@gmail.com>
2024-09-08 17:35:25 +0300

    adds a count

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self._value = value
+    
+  @property 
+  def value(self):
+    return self._value
+  
+  @value.setter 
+  def value(self, new_value):
+    if type(new_value) == str:
+      self._value = new_value
+    else:
+      print("The value must be a string.")  
+  
+  
+  def is_sentence(self):
+    return self._value.endswith(".")
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+    value = self._value
+    for punc in ['!','?']:
+        value = value.replace(punc, '.')
+    sentences = [string for string in value.split('.') if string]
+    
+    return len(sentences)  
